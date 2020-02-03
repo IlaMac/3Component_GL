7e9fd4a2fcad130645dd625d27e5d1ba4f37b59e
IlaMac <ilariam@kth.se>
2020-02-03 13:49:38 +0100

    new batch_scripts

--- batch_script/LogBoxing.py
+++ batch_script/LogBoxing.py
@@ -1,6 +1,6 @@
 import numpy as np
-#import matplotlib.pyplot as plt
-#import matplotlib.colors as mcolors
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
 import sys
 import os
 import math
@@ -9,17 +9,16 @@ BASEDIR=str(sys.argv[1])
 A_name=str(sys.argv[2])
 nbeta=int(sys.argv[3])
 
-print(nbeta)
-
-base=2
-exp=9
-
-box_length=base**exp
-print(box_length)
 #the generic observable is called here A
-
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif', size='18')
+plt.rc('text.latex', preamble=r'\usepackage{bm}')
 
 for b in range(nbeta):
+
+    base=2
+    exp=2
+    box_length=base**exp
 
     file_A=("%s/beta_%d/%s.txt" %(BASEDIR, b, A_name))
     A=np.loadtxt(file_A, usecols=1, unpack=True)
@@ -30,24 +29,35 @@ for b in range(nbeta):
     A_std=[]
     bins=[]
     while (tot_length< len(A)):
-    
-        A_mean.append=np.mean(A[start:tot_length])
-        A_std.append=np.std(A[start:tot_length)], axes=0)/np.sqrt(box_length -1)
-        bins.append=tot_length
+        
+        print(start)
+        print(tot_length)
+        print(len(A))
+        A_mean.append(np.mean(A[start:tot_length]))
+        A_std.append(np.std(A[start:tot_length]))
+        bins.append(tot_length)
 
-        print(box_length)
-        print(tot_length -start)
-
-        start+=box_length
+        start=tot_length
         exp+=1
         box_length=base**exp
         tot_length+=box_length
     A_mean=np.array(A_mean)
     A_std=np.array(A_std)
     bins=np.array(bins)
+    print(A_mean)
     file_Aout=("%s/beta_%d/Thermalization_%s.txt" %(BASEDIR, b, A_name))
     np.savetxt(file_Aout, np.transpose( [bins, A_mean, A_std]), fmt='%19.12e', delimiter=' ',  newline=os.linesep)
+    
+    fig, ((ax1))= plt.subplots(1, 1)
+    ax1.set_title("beta "+str(b))
+    ax1.errorbar(bins, A_mean,yerr=A_std, fmt='o')
+    ax1.grid(True)
+    ax1.set_xlabel(r'$t_{MC}$')
+    ax1.set_xscale("log")
+    ax1.set_ylabel("$%s$" %A_name)
+    plt.tight_layout()
+    plt.show()
 
-       
+  
 
 
